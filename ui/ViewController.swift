1042f7202f64676475933766f3a6267df866d21b
frh <frh.shaalan@gmail.com>
2022-03-15 03:48:13 +0300

    Initial Commit

---
+++ ui/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ui
+//
+//  Created by frh alshaalan on 12/08/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
